bcbab096f2eed7ab740399e45a02cda5401820b2
Arne Schauf <git@as-webservices.de>
2015-01-14 19:44:22 +0100

    fix not found config

---
+++ dockerfiles/0.9.2/config.py
@@ -0,0 +1,21 @@
+DEBUG = False
+
+LOG_FILE = 'logs/app.log'
+
+# If set to false, flask's url handling will be used for external libraries.
+# The static directory contains a script to download all dependencies.
+USE_CDN = True
+# If set to false, no liveupdates, no websockets
+USE_LIVEUPDATES = True
+
+# the function list shown in the webinterface's navbar.
+FUNCTION_QUICKLIST = ['state.highstate', 'state.sls', 'pkg.upgrade', 'test.ping']
+# the default redirect when visiting /
+DEFAULT_FUNCTION = 'state.highstate'
+
+import os
+
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+REDIS_DB = 0
+REDIS_PASS = None
